ad47499173ed58acc27fb09f58f3c4b4f5e844f5
sepulturesa <61355011+SepultureSE@users.noreply.github.com>
2021-02-17 15:52:17 +0300

    Удален ненужный скрипт для Telegram бота
    Тоже попал туда случайно

--- project/bot.py
+++ project/bot.py
@@ -1,13 +1,1 @@
-import telebot
-from lightshotparser import LightshotParser
 
-
-bot = telebot.TeleBot(token='1636954625:AAF2-s8UFPTLE3i3KZQAS41IdvzvApTNkko')
-
-@bot.message_handler(commands=['start'])
-def on_start(message):
-    screenshot = LightshotParser.get_random_screenshot()
-    bot.send_photo(message.from_user.id, screenshot.image)
-    
-    
-bot.polling()
